4696470486874c5f387e4047f27764798fe591a5
cuberkam <echocodereko@gmail.com>
2022-09-03 20:45:24 +0300

    Add users form and views

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,33 @@
+from django import forms
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
 from .models import CustomUser
+
+
+class LoginForm(forms.Form):
+    email = forms.CharField(label="Email")
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+
+
+class RegisterForm(forms.Form):
+    email = forms.CharField(max_length=50, label="Email")
+    password = forms.CharField(
+        max_length=20, label="Password", widget=forms.PasswordInput
+    )
+    password_confirm = forms.CharField(
+        max_length=20, label="Password Confirm", widget=forms.PasswordInput
+    )
+
+    def clean(self):
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        password_confirm = self.cleaned_data.get("password_confirm")
+
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError("Parolalar Eşleşmiyor")
+
+        values = {"username": email, "password": password}
+        return values
 
 
 class CustomUserCreationForm(UserCreationForm):
--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def login(request):
+    return render(request, "login.html")
+
+
+def register(request):
+    return render(request, "register.html")
+
+
+def logout(request):
+    pass
