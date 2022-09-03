e5bf3130acb89a4447844385df5af5842df0a50d
cuberkam <echocodereko@gmail.com>
2022-09-03 20:52:09 +0300

    Add static and template to settings

--- wordyforest/settings.py
+++ wordyforest/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = "wordyforest.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -108,7 +108,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
