60b9a8e25a4def929dc6e4809e18640a0447a3fe
cuberkam <echocodereko@gmail.com>
2022-09-06 10:35:07 +0300

    Update urls and settings

--- wordyforest/settings.py
+++ wordyforest/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "users.apps.UsersConfig",
+    "main_page.apps.MainPageConfig",
+    "word_lists.apps.WordListsConfig",
 ]
 
 MIDDLEWARE = [
--- wordyforest/urls.py
+++ wordyforest/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from main_page import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.index, name="index"),
+    path("", include("main_page.urls")),
     path("auth/", include("users.urls")),
 ]
